c5aac2e336fcc0630a1e081bb8f14583bf2b92b6
fourastiemarvin <marvin.fourastie@etu.unige.ch>
2021-03-21 18:43:53 +0100

    fix script which produce the maude module

--- maude-3.1/scripts/parser.py
+++ maude-3.1/scripts/parser.py
@@ -21,6 +21,10 @@ eqDeclList = []
 # contains elements for type declaration (aka parametrized module)
 typeDeclList = []
 
+includeList = []
+
+sortList = []
+
 # FIXME: the type of the right term is not inferred (typed as any)
 # entrypoint, parse each case
 def parseCase(prog):
@@ -82,16 +86,13 @@ def opDecl(label, arity, parent=None, pos=None):
     for i in range(arity):
         opList.append("any")
     # the last element is the co-domain (to be infered)
+    # print(typedList)
     for t in typedList:
         if parent == t[0]:
             opList.append(t[pos])
             return opList
     opList.append("any")
     return opList
-
-# var = [['x', 'any'], ['y', 'any']]
-# op = [['false', 'T'], ['id', 'any', 'T'], ['if', 'T', 'List', 'List'], ['true', 'any']]
-# typed = [['ajoute', 'T', 'List', 'List'], ['aj', 'T', 'List', 'List']]
 
 # build eq for the maude module using eqDeclList
 def eqDecl():
@@ -126,16 +127,20 @@ def getType(typeDict):
         # case with no parameter (the co-domain is always the last element of v)
         if len(v) == 1:
             typedList.append([k, v[-1][-1]])
+            sortList.append(v[-1][-1])
         else:
             curRuleList = []
             curRuleList.append(k)
             for t in v[0:len(v)]:
                 curRuleList.append(t[-1])
+                sortList.append(t[-1])
             typedList.append(curRuleList)
 
 # return declaration of the view and the module for the given type
 def initType():
     global typeDeclList
+    global includeList
+    global sortList
     viewDecl = ""
     typeMod = "fmod "
     for t in typeDeclList:
@@ -144,12 +149,23 @@ def initType():
             if item == "":
                 nameList.remove(item)
         typeMod += nameList[0] + "{ X :: TRIV } is \n sort " + nameList[0] + "{ X } . \n"
+        includeList.append(nameList[0] + "{" + nameList[-1] + "}")
+        if nameList[0] in sortList:
+            sortList.remove(nameList[0])
+        for tyIt in opDeclList:
+            for el in tyIt:
+                if nameList[0] == el:
+                    tyIt[tyIt.index(el)] = nameList[0] + "{" + nameList[-1] + "}"
         viewDecl += "view " + nameList[-1] + " from TRIV to QID is sort Elt to Qid . endv \n"
+        sortList.append(nameList[-1])
         for ctor in t[1:]:
             func = ctor.split(" ")
             for item in func:
                 if item == "":
                     func.remove(item)
+            for op in opDeclList:
+                if func[0] in op:
+                    opDeclList.remove(op)
             if len(func) == 1:
                 typeMod += " op " + func[0] + " : -> " + nameList[0] + "{ X } . \n"
             else:
@@ -165,32 +181,40 @@ def initType():
 
 # init the module write declarations of function in funrules.maude
 def buildModule():
-    # contains elements for var declaration
     global varDeclList
-    # contains elements for op declaration
     global opDeclList
-    # contains typed terms
     global typedList
-    # contains elements for eq declaration
     global eqDeclList
-    # contains elements for type declaration (aka parametrized module)
     global typeDeclList
-    decl = ""
-    # d = ast.literal_eval(data)
-    # iterate over keys k and values v
-    # for k, v in d.items():
-    #     if len(v) == 1:
-    #         decl = decl + "op " + k + " : " + "-> " + v[-1][-1] + " . \n"
-    #     else:
-    #         dom = ' '.join([t[-1] for t in v[0:len(v)]])
-    #         decl = decl + "op " + k + " : " + dom + " -> " + v[-1][-1] + " . \n"
+    global includeList
+    global sortList
+    decl = "fmod FUNRULES is \n protecting QID . \n sort any . \n"
+    for s in includeList:
+        decl += "including " + s + " . \n"
+        decl += "subsort any < " + s + " . \n"
+    for s in sortList:
+        decl += "sort " + s + " . \n subsort any < "  + s + " < Qid . \n"
+    for v in varDeclList:
+        decl += "var $" + v[0] + " : " + v[-1] + " . \n"
+    for op in opDeclList:
+        decl += "op " + op[0] + " :"
+        for s in op[1:-1]:
+            decl += " " + s + " "
+        decl += " -> " + op[-1] + " . \n"
+    for eq in eqDeclList:
+        decl += eq + "\n"
+    decl += "endfm"
     with open("../funblocks/funrules.maude", 'w') as file :
         file.write(initType()[0] + "\n")
         file.write(initType()[1] + "\n")
-        file.write("fmod FUNRULES is \n")
         file.write(decl)
 
+data = '{ "depth" : [ ( "TypeDeclRef" , "Tree" ) , ( "TypeDeclRef" , "Nat" ) ] }'
+getType(data)
+parseCase(parsed)
+eqDecl()
 typeDecl()
+initType()
 buildModule()
 
 #############################   TESTS ###########################################
@@ -210,3 +234,5 @@ buildModule()
 # typeDecl()
 # print("type: ", typeDeclList)
 # # [' Tree $T ', ' empty ', ' leaf $T ', ' node (Tree $T) (Tree $T)']
+# print("sort: ",sortList)
+# print("inc: ", includeList)
