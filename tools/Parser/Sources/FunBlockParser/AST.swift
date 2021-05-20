fd2eb97e2a9dd6404a6cce2812591e3dbbd76408
fourastiemarvin <marvin.fourastie@etu.unige.ch>
2021-05-20 11:55:40 +0200

    add gitignore

---
+++ tools/Parser/Sources/FunBlockParser/AST.swift
@@ -0,0 +1,263 @@
+public protocol AST {}
+
+var signArray: [String:[String]] = [:]
+
+public struct TypeDecl: AST, CustomStringConvertible {
+
+  public let name: String
+
+  public let parameters: [TypeVarRef]
+
+  public let cases: [TypeCons]
+
+  public var description: String {
+    var result = "type \(name)"
+    if !parameters.isEmpty {
+      result += " " + parameters.map(String.init(describing:)).joined(separator: " ")
+    }
+    result += " :: " + cases.map(String.init(describing:)).joined(separator: " | ")
+    return result
+  }
+
+  public var toMaude: String {
+    var result = ""
+    var sortName = name
+    // TODO: use {} to avoid sort overriding ???
+    if !parameters.isEmpty {
+      for p in parameters {
+        result += "(view \(p) from TRIV to \(name) is sort Elt to \(name) . endv) \n"
+      }
+      let fmodName = parameters.map(String.init(describing:)).joined(separator: " :: TRIV, ") + " :: TRIV"
+      sortName = name + "{" + parameters.map(String.init(describing:)).joined(separator: ",") + "}"
+      result += "(fmod \(name){\(fmodName)} is \n sort \(sortName) . \n"
+    }
+    else {
+      result += "(fmod \(name) is \n sort \(sortName) . \n"
+    }
+    for c in cases {
+      result += "op \(c.label) : "
+      signArray[c.label] = []
+      if !c.arguments.isEmpty {
+        for arg in c.arguments {
+          if let varElt = arg as? TypeVarRef {
+            result += varElt.toMaude + " "
+            signArray[c.label]!.append(varElt.toMaude)
+          }
+          else if let declElt = arg as? TypeDeclRef {
+            result += declElt.toMaude + " "
+            signArray[c.label]!.append(declElt.toMaude)
+          }
+        }
+      }
+      result += "-> \(sortName) . \n"
+    }
+    result += "endfm) \n"
+    return result
+  }
+
+}
+
+public struct InitStateDecl: AST, CustomStringConvertible {
+
+  public let state: Expr
+
+  public var description: String {
+    "init \(state)"
+  }
+
+}
+
+public struct RuleDecl: AST, CustomStringConvertible {
+
+  public let name: String
+
+  public let arguments: [TypeRef]
+
+  public let left: TypeSign
+
+  public let right: TypeSign
+
+  public var description: String {
+    let tail = " :: \(left) => \(right)"
+    return arguments.isEmpty
+      ? "decl \(name)" + tail
+      : "decl \(name) " + arguments.map({ "(\($0))" }).joined(separator: " ") + tail
+  }
+  public var toMaude: String {
+    var result = ""
+    if !arguments.isEmpty {
+      for arg in arguments {
+        result += "sort \(arg) .\n"
+      }
+    }
+    signArray[name] = []
+    let righth = right as! TypeDeclRef
+    if let lefth = left as? ArrowTypeSign {
+      result += "op \(name) : \(lefth.toMaude) -> \(righth.toMaude) .\n "
+      signArray[name]! += lefth.toMaude.components(separatedBy: " ").filter(){$0 != ""}
+    }
+    else if let lefth = left as? TypeDeclRef {
+      result += "op \(name) : \(lefth.toMaude) -> \(righth.toMaude) .\n "
+      signArray[name]! += lefth.toMaude.components(separatedBy: " ").filter(){$0 != ""}
+    }
+    return result
+  }
+
+}
+
+public struct RuleDef: AST, CustomStringConvertible {
+
+  public let left: Expr
+
+  public let right: Term
+
+  public var description: String {
+    "rule \(left) => \(right)"
+  }
+  public var toMaude: String {
+    var result = ""
+    result += left.toMaude
+    if let righth = right as? Expr {
+      result += righth.toMaude
+    }
+    result += "eq \(left) = \(right) ."
+    return result
+  }
+
+}
+
+public struct TypeCons: AST, CustomStringConvertible {
+
+  public let label: String
+
+  public let arguments: [TypeRef]
+
+  public var description: String {
+    arguments.isEmpty
+      ? label
+      : label + " " + arguments.map({ "(\($0))" }).joined(separator: " ")
+  }
+
+}
+
+public protocol TypeSign: AST {}
+
+public struct ArrowTypeSign: TypeSign, CustomStringConvertible {
+
+  let left: TypeSign
+
+  let right: TypeSign
+
+  public var description: String {
+    "(\(left)) -> (\(right))"
+  }
+
+  public var toMaude: String {
+    var result = ""
+    if let lefth = left as? TypeDeclRef {
+      result += "\(lefth.toMaude) "
+    }
+    else if let lefth = left as? ArrowTypeSign {
+      result += "\(lefth.left) \(lefth.right)"
+    }
+    if let righth = right as? TypeDeclRef {
+      result += "\(righth.toMaude) "
+    }
+    else if let righth = right as? ArrowTypeSign {
+      result += "\(righth.left) \(righth.right)"
+    }
+    return result
+  }
+
+}
+
+public protocol TypeRef: TypeSign {}
+
+public struct TypeDeclRef: TypeRef, CustomStringConvertible {
+
+  public let name: String
+
+  public let arguments: [TypeRef]
+
+  public var description: String {
+    arguments.isEmpty
+      ? name
+      : name + " " + arguments.map({ "(\($0))" }).joined(separator: " ")
+  }
+
+  public var toMaude: String {
+    name + arguments.map({ "{\($0)}" }).joined(separator: " ")
+  }
+
+}
+
+public struct TypeVarRef: TypeRef, CustomStringConvertible {
+
+  public let name: String
+
+  public var description: String {
+    "$" + name
+  }
+
+  public var toMaude: String {
+    "$" + name + "$Elt"
+  }
+
+}
+
+public protocol Term: AST {}
+
+public struct Expr: Term, CustomStringConvertible {
+
+  public let label: String
+
+  public let subterms: [Term]
+
+  public var description: String {
+    subterms.isEmpty
+      ? label
+      : label + "(" + subterms.map(String.init(describing:)).joined(separator: ", ") + ")"
+  }
+
+  public var toMaude: String {
+    var result = ""
+    var id1 = -1
+    for sub in subterms {
+      id1 += 1
+      if let v = sub as? VarRef {
+        let sort = signArray[self.label]![id1]
+        result += v.toMaude + sort + " . \n"
+      }
+      else if let t = sub as? Expr {
+        if !t.subterms.isEmpty {
+          var id2 = -1
+          for sub in t.subterms {
+            id2 += 1
+            if let subt = sub as? Expr {
+              result += subt.toMaude
+            }
+            else if let subt = sub as? VarRef {
+              let sort = signArray[self.label]![id1]
+              result += subt.toMaude + sort + " . \n"
+            }
+          }
+        }
+      }
+    }
+    return result
+  }
+}
+
+public struct VarRef: Term, CustomStringConvertible {
+
+  public let name: String
+
+  public var description: String {
+    "$" + name
+  }
+
+  public var toMaude: String {
+    let result = "var $\(name): "
+    return result
+  }
+}
---
+++ tools/Parser/Sources/FunBlockParser/main.swift
@@ -0,0 +1,64 @@
+var src =
+"""
+// Type definitions
+type List $T :: empty | cons $T (List $T)
+
+// Initial state
+init cons(1, cons(2, empty))
+
+// Quicksort
+rule sort :: List Int => List Int
+case sort(empty) => empty
+case sort(cons($x, $y)) => cat(filter_lt($x, $y), cons($x, sort(filter_gt($x, $y))))
+
+// Filters
+rule filter_lt :: Int -> List Int => List Int
+case filter_lt($x, empty) => empty
+case filter_lt($x, cons($y, $z)) => if(lt($y, $x), cons($y, filter_lt($z)), filter_lt($z))
+
+rule filter_gt :: Int -> List Int => List Int
+case filter_gt($x, empty) => empty
+case filter_gt($x, cons($y, $z)) => if(gt($y, $x), cons($y, filter_gt($z)), filter_gt($z))
+
+// Conditional expressions
+rule if $T :: Bool -> $T -> $T => $T
+case if(true, $x, $y) => $x
+case if(false, $x, $y) => $y
+"""
+
+FunBlockParser.initialize()
+
+// let tokens = try tokenize(src)
+// let stmts = FunBlockParser.parse(tokens)
+// for stmt in stmts {
+//   print(stmt)
+// }
+
+let prog =
+"""
+type Nat :: zero | succ Nat
+type Tree $T :: empty | leaf $T | node (Tree $T) (Tree $T)
+rule depth $T :: Tree $T => Nat
+rule quicksort :: List $T -> Int => List Int
+rule add :: Nat -> Nat => Nat
+case add($x, add($y, zero)) => $x
+case depth(empty) => zero
+case succ($z) => zero
+"""
+
+let tokens = try tokenize(prog)
+let stmts = FunBlockParser.parse(tokens)
+for stmt in stmts {
+  if let s = stmt as? TypeDecl {
+    print(s.toMaude)
+  }
+  else if let s = stmt as? RuleDecl {
+    print(s.toMaude)
+  }
+  else if let s = stmt as? RuleDef {
+    print(s.toMaude)
+  }
+  else {continue}
+}
+print("\n TEST:")
+print(signArray)
